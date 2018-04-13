24ecfbc8f1dcb3930de9732f4b392729d01e89b1
Homme <36703853+INatsuI@users.noreply.github.com>
2018-04-13 18:19:10 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -8,7 +8,7 @@ bot.on('ready', function() {
     console.log("Go");
 
 bot.on("guildMemberAdd", member => {
-    let role = member.guild.roles.find("name", "Utilisateur")
+    let role = member.guild.roles.find("name", "Membre")
     member.guild.channels.find("name", "bienvenue").send(`Bienvenue  @${member.user.username} sur le discord de Dédé !`)
     member.addRole(role)
 
